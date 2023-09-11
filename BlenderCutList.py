d7c00608ee58686e51501e8a3787c9c7cd4339aa
Filipe Lopes <contato@filipelopes.me>
2023-09-11 13:02:42 +0000

    refactor: adapt old code to cortecloud specifications and create object oriented classes

--- BlenderCutList.py
+++ BlenderCutList.py
@@ -1,5 +1,6 @@
-#(c) 2022 Tim Huckaby (@timhuckaby)
-#
+# (c) 2023 Filipe Lopes (@filiperochlopes) PT_BR 
+# (c) 2022 Tim Huckaby (@timhuckaby)
+
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
@@ -18,54 +19,100 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+# ##### Versão PT_BR por Filipe Lopes #####
+#
+#  O código foi atualizado conforme proposto inicialmente por 
+#  Tim Huckaby para bom funcionamento com o Cortecloud, muito utilizado
+#  no Brasil para corte planejados, ficando esse plugin como uma opção
+#  OpenSource para medidas de plano de corte
+# 
+# ##### Fim do Changelog #####
 
+# Importando a API do blender
+import bpy, os, re
 
-# import the blender's python API and python's os module
-import bpy, os
-
-
-# select all the mesh objects in the scene
-objects = bpy.context.scene.objects
-for ob in objects:
-    ob.select_set(ob.type == "MESH")
-
-# get the current selection
+# Captura apenas a seleção, gosto de utilizar coleções de madeira e outras de outros materiais, dessa forma fica fácil selecionar apenas os itens de madeira
 selection = bpy.context.selected_objects
 
-# initialize a blank result variable
-result = ""
+# Arquivo de saída (edite aqui)
+output_file = "Desktop/CutList.csv"
+# string zerada para popular com dados csv
+csv_output = ""
+# lista de nome de objetos que não deverão ser analisados, pois são duplicatas de outros objetos. São esses os de final *.001, *.002 ...
+blacklist = []
 
-# iterate through the selected objects
-for sel in selection:
-    # get the current object's dimensions; convert from meters to inches and round to 2 decimal places
-    dims = sel.dimensions
-    x = float(round(sel.dimensions.x * 39.370, 2))
-    y = float(round(sel.dimensions.y * 39.370, 2))
-    z = float(round(sel.dimensions.z * 39.370, 2))
-    # format and output to the screen
-    scr = "%s - %.02fin x %.02fin x %.02fin\n" % (sel.name, x, y, z)
-    print(scr)
-    # write the selected object's name and dimensions to a string for the file output
-    # if you want to output to a txt file to open in word, notepad, etc. then use this line:
-    # result += "%s - %.02fin x %.02fin x %.02fin\n" % (sel.name, x, y, z)
-    # if you want to output to a csv file to open in excel so you can sort, etc. then use this line:
-    result += "%s , %.02f , %.02f , %.02f\n" % (sel.name, x, y, z)
+# classe para organizar a fitagem de borda de uma peça
+class EdgeTapes:
+    def __init__(self, material_names):
+        def get_tape_material(tape_identifier:str):
+            l = [m for m in material_names if tape_identifier in m]
+            return l[0] if len(l) > 0 else None
+            
+        # Verifica se existem algum material com as palavras chaves
+        self.c1 = get_tape_material('C1')
+        self.c2 = get_tape_material('C2')
+        self.l1 = get_tape_material('L1')
+        self.l2 = get_tape_material('L2')
+    
+    def __str__(self):
+        str = ''
+        if self.c1:
+            str += f'C1: {self.c1} '
+        if self.c2:
+            str += f'C2: {self.c2} '
+        if self.l1:
+            str += f'L1: {self.l1} '
+        if self.l2:
+            str += f'L2: {self.l2} '
+        return str
+        
+# classe para organizar as características de um corte do material
+class WoodenPiece:
+    def __init__(self, dimensions:list, material_names:list, name:str=None, comments:str=None):
+        self.name = name
+        self.comments = comments
+        self.thickness = min(dimensions)
+        dimensions.remove(self.thickness)
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+        self.material = self.get_main_material_from_materials(material_names)
+        self.edge_tapes = EdgeTapes(material_names)
+    
+    def get_main_material_from_materials(self, material_names):
+        for m in material_names:
+            if re.search("^((?!C1|C2|L1|L2|Fita).)*$", m):
+                return f'{m} {self.thickness}mm'
+        return None
+    
+    def __str__(self):
+        return f'''
+        Corte de MDF "{self.name}" - {self.comments} ({self.material})
+        Dimensões (mm) {self.width}mm x {self.height}mm Espessura {self.thickness}mm
+        Fitas: {self.edge_tapes}
+        '''
 
-# get path to render output (ie: C:\Users\TimHuckaby\)
-tempFolder = os.path.expanduser('~')
+# Cria o arquivo de saída no diretório "~/Desktop/CutList.csv"
+user_folder = os.path.expanduser('~')
 # make a filename
-# if you are creating a text file for word, notepad, etc uncomment this next line:
-# filename = os.path.join (tempFolder, "Documents\CutList.txt")
-# if you are creating a csv file for excel uncomment this next line:
-filename = os.path.join (tempFolder, "Documents\CutList.csv")
+filename = os.path.join (user_folder, output_file)
 # confirm path exists
 os.makedirs(os.path.dirname(filename), exist_ok=True)
 # open the file to write to
 file = open(filename, "w")
-# write the data to file
-# write the header for excel.  if you are creating a txt file you can comment out this next line:
-file.write("Name, Length, Width, Height\n")
-file.write(result)
-# close the file
-file.close()
+# Escreve o cabeçalho do arquivo
+file.write("Quantidade, Comprimento, Largura, Função, Fita C1, Fita C2, Fita L1, Fita L2, Material, Complemento\n")
+# iterate through the selected objects
+for sel in selection:
+    if sel.name not in blacklist:
+        quantity = 0
+        # captura o nome e as dimensões da peça selecionada
+        wooden_piece = WoodenPiece(name=sel.name, dimensions=[int(sel.dimensions.x*1000), int(sel.dimensions.y*1000), int(sel.dimensions.z*1000)], material_names=[m.name for m in sel.material_slots], comments=sel['comments'] if 'comments' in sel else None)
+        # Verifica se temos outra peça com o mesmo nome e *.001 para adicionar na quantidade
+        for s in selection:
+            if(re.search(sel.name+r"\.\d{3}$", s.name)):
+                blacklist.append(s.name)
+                quantity += 1
+        # Adiciona a linha ao csv
+        file.write(f"{quantity}, {wooden_piece.width}, {wooden_piece.height}, {wooden_piece.name}, {wooden_piece.edge_tapes.c1}, {wooden_piece.edge_tapes.c2}, {wooden_piece.edge_tapes.l1}, {wooden_piece.edge_tapes.l2}, {wooden_piece.material}, {wooden_piece.comments}\n")
 
+file.close()
