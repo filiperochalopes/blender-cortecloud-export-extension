9c85ee28f70126d7703679669c3dcc4f006775de
Filipe Lopes <filiperocklopes@gmail.com>
2024-09-15 12:09:50 -0300

    lint: ruff formating

--- BlenderCutList.py
+++ BlenderCutList.py
@@ -1,44 +1,84 @@
+# (c) 2023 Filipe Lopes (@filiperochlopes) PT_BR
+# (c) 2022 Tim Huckaby (@timhuckaby)
+
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# ##### Versão PT_BR por Filipe Lopes #####
+#
+#  O código foi atualizado conforme proposto inicialmente por
+#  Tim Huckaby para bom funcionamento com o Cortecloud, muito utilizado
+#  no Brasil para corte planejados, ficando esse plugin como uma opção
+#  OpenSource para medidas de plano de corte
+#
+# ##### Fim do Changelog #####
+
+# Importando a API do blender
 import bpy, os, re
 
-# Captura apenas a seleção de objetos no Blender
+# Captura apenas a seleção, gosto de utilizar coleções de madeira e outras de outros materiais, dessa forma fica fácil selecionar apenas os itens de madeira
 selection = bpy.context.selected_objects
 
-# Arquivo de saída (edite aqui se necessário)
-output_file = 'Desktop/CutList.csv'
-
-# Lista de objetos que não serão analisados (duplicatas)
+# Arquivo de saída (edite aqui)
+output_file = "Desktop/CutList.csv"
+# string zerada para popular com dados csv
+csv_output = ""
+# lista de nome de objetos que não deverão ser analisados, pois são duplicatas de outros objetos. São esses os de final *.001, *.002 ...
 blacklist = []
 
-# Classe para organizar a fitagem de borda de uma peça
+
+# classe para organizar a fitagem de borda de uma peça
 class EdgeTapes:
     def __init__(self, material_names):
         def get_tape_material(tape_identifier: str):
             l = [m for m in material_names if tape_identifier in m]
-            return l[0].replace(tape_identifier, '').strip() if len(l) > 0 else None
+            return l[0].replace(tape_identifier, "").strip() if len(l) > 0 else None
 
-        # Verifica se existem materiais com identificadores de fitas
-        self.c1 = get_tape_material('C1')
-        self.c2 = get_tape_material('C2')
-        self.l1 = get_tape_material('L1')
-        self.l2 = get_tape_material('L2')
+        # Verifica se existem algum material com as palavras chaves
+        self.c1 = get_tape_material("C1")
+        self.c2 = get_tape_material("C2")
+        self.l1 = get_tape_material("L1")
+        self.l2 = get_tape_material("L2")
 
     def __str__(self):
-        str = ''
+        str = ""
         if self.c1:
-            str += f'C1: {self.c1} '
+            str += f"C1: {self.c1} "
         if self.c2:
-            str += f'C2: {self.c2} '
+            str += f"C2: {self.c2} "
         if self.l1:
-            str += f'L1: {self.l1} '
+            str += f"L1: {self.l1} "
         if self.l2:
-            str += f'L2: {self.l2} '
+            str += f"L2: {self.l2} "
         return str
 
-# Classe para organizar as características de um corte do material
+
+# classe para organizar as características de um corte do material
 class WoodenPiece:
-    def __init__(self, dimensions: list, material_names: list, name: str = None, role: str = None):
+    def __init__(
+        self,
+        dimensions: list,
+        material_names: list,
+        name: str = None,
+        notes: str = None,
+    ):
         self.name = name
-        self.role = role
+        self.notes = notes
         self.thickness = min(dimensions)
         dimensions.remove(self.thickness)
         self.width = dimensions[0]
@@ -49,54 +89,57 @@ class WoodenPiece:
     def get_main_material_from_materials(self, material_names):
         for m in material_names:
             if re.search("^((?!C1|C2|L1|L2|Fita).)*$", m):
-                return f'{m} {self.thickness}mm'
+                return f"{m} {self.thickness}mm"
         return None
 
     def __str__(self):
-        return f'''
-        Corte de MDF "{self.name}" - {self.role} ({self.material})
+        return f"""
+        Corte de MDF "{self.name}" - {self.notes} ({self.material})
         Dimensões (mm) {self.width}mm x {self.height}mm Espessura {self.thickness}mm
         Fitas: {self.edge_tapes}
-        '''
+        """
 
-# Caminho do arquivo de saída
-user_folder = os.path.expanduser('~')
+
+# Cria o arquivo de saída no diretório "~/Desktop/CutList.csv"
+user_folder = os.path.expanduser("~")
+# make a filename
 filename = os.path.join(user_folder, output_file)
+# confirm path exists
 os.makedirs(os.path.dirname(filename), exist_ok=True)
+# open the file to write to
+file = open(filename, "w")
+# Escreve o cabeçalho do arquivo
+file.write(
+    "Quantidade;Comprimento;Largura;Função;Fita C1;Fita C2;Fita L1;Fita L2;Material;Complemento\n"
+)
+# iterate through the selected objects
+for sel in selection:
+    if re.search(r"\.\d{3}$", sel.name):
+        continue
+    if sel.name not in blacklist:
+        quantity = 1
+        # captura o nome e as dimensões da peça selecionada
+        wooden_piece = WoodenPiece(
+            name=sel.name,
+            dimensions=[
+                int(sel.dimensions.x),
+                int(sel.dimensions.y),
+                int(sel.dimensions.z),
+            ],
+            material_names=[m.name for m in sel.material_slots],
+            notes=sel["notes"] if "notes" in sel else None,
+        )
+        # Verifica se temos outra peça com o mesmo nome e *.001 para adicionar na quantidade
+        for s in selection:
+            if re.search(sel.name + r"\.\d{3}$", s.name):
+                blacklist.append(s.name)
+                quantity += 1
+        # O comprimento deve ser sempre o maior tamanho, para que a idea ddas fitas funcionem
+        width = max(wooden_piece.width, wooden_piece.height)
+        height = min(wooden_piece.width, wooden_piece.height)
+        # Adiciona a linha ao csv
+        file.write(
+            f"{quantity};{width};{height};{wooden_piece.name};{wooden_piece.edge_tapes.c1 or ''};{wooden_piece.edge_tapes.c2 or ''};{wooden_piece.edge_tapes.l1 or ''};{wooden_piece.edge_tapes.l2 or ''};{wooden_piece.material};{wooden_piece.notes or ''}\n"
+        )
 
-# Abre o arquivo para escrita
-with open(filename, "w") as file:
-    # Escreve o cabeçalho
-    file.write("Quantidade;Comprimento;Largura;Função;Fita C1;Fita C2;Fita L1;Fita L2;Material;Role\n")
-    
-    # Itera sobre os objetos selecionados
-    for sel in selection:
-        if re.search(r"\.\d{3}$", sel.name):
-            continue
-        if sel.name not in blacklist:
-            quantity = 1
-            # Captura a propriedade "role" do objeto, se existir
-            role = sel.get('role', None)
-            # Cria uma instância de WoodenPiece com as dimensões e materiais
-            wooden_piece = WoodenPiece(
-                name=sel.name, 
-                dimensions=[int(sel.dimensions.x), int(sel.dimensions.y), int(sel.dimensions.z)], 
-                material_names=[m.name for m in sel.material_slots], 
-                role=role
-            )
-            
-            # Verifica duplicatas com o mesmo nome (ex.: *.001) para ajustar a quantidade
-            for s in selection:
-                if re.search(sel.name + r"\.\d{3}$", s.name):
-                    blacklist.append(s.name)
-                    quantity += 1
-
-            # O comprimento deve ser sempre o maior tamanho
-            width = max(wooden_piece.width, wooden_piece.height)
-            height = min(wooden_piece.width, wooden_piece.height)
-            
-            # Adiciona uma linha ao CSV
-            file.write(f"{quantity};{width};{height};{wooden_piece.name};"
-                       f"{wooden_piece.edge_tapes.c1 or ''};{wooden_piece.edge_tapes.c2 or ''};"
-                       f"{wooden_piece.edge_tapes.l1 or ''};{wooden_piece.edge_tapes.l2 or ''};"
-                       f"{wooden_piece.material};{wooden_piece.role or ''}\n")
+file.close()
